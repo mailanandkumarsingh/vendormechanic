c8b7fb4d4e6e9896c5a918718c874fa6f8b5a4fb
Anand <andyaks83@gmail.com>
2019-01-24 16:09:34 +0530

    Updates

---
+++ precache-manifest.a2529c5cb1bb15e28f14f5a668eee2a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad4418414f6bbed9fe1833dd249e3b29",
+    "url": "/vendormechanic/static/media/twix.ad441841.jpg"
+  },
+  {
+    "revision": "b26a8fe91ee6e20391b032bd2965102b",
+    "url": "/vendormechanic/static/media/oreo.b26a8fe9.jpg"
+  },
+  {
+    "revision": "b08590fbf25334bd9924e2f6afa1b560",
+    "url": "/vendormechanic/static/media/lays.b08590fb.jpg"
+  },
+  {
+    "revision": "6814c45489cd7214341413c9059dd273",
+    "url": "/vendormechanic/static/media/coke.6814c454.jpg"
+  },
+  {
+    "revision": "81bf420f972b3a80ad1d",
+    "url": "/vendormechanic/static/js/runtime~main.81bf420f.js"
+  },
+  {
+    "revision": "c4c2b45605d32504ea0b",
+    "url": "/vendormechanic/static/js/main.c4c2b456.chunk.js"
+  },
+  {
+    "revision": "9304a076addff40a2766",
+    "url": "/vendormechanic/static/js/1.9304a076.chunk.js"
+  },
+  {
+    "revision": "c4c2b45605d32504ea0b",
+    "url": "/vendormechanic/static/css/main.89ef5111.chunk.css"
+  },
+  {
+    "revision": "78208d914ab5d7a9885fbec290815e5f",
+    "url": "/vendormechanic/index.html"
+  }
+];
